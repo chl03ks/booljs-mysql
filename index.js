8fd3e59de2188288316f453e5de2bf5c684e4cf0
Pablo Andres Dorado Suarez <im@pandres95.me>
2016-05-11 17:27:18 -0500

    Update plugin

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 'use strict';
 
-var API = require('bool.js/api');
+const API = require('booljs-api')
+,     lib = require('./lib');
 
-module.exports = new API.DatabaseLoader(
-    'booljs-mysql', // Name
-    require('./lib') // Functions
-);
+module.exports = class BoolJSMySQL extends API.DatabaseLoader {
+    constructor() {
+        super('booljs-mysql');
+    }
+
+    openDatabase(configuration){
+        return lib.openDatabase(configuration);
+    }
+
+    fetchModels(_instance, models, connection){
+        return lib.fetchModels(_instance, models, connection);
+    }
+
+    modelTemplate(){
+        return lib.modelTemplate();
+    }
+
+    modelConfiguration(){
+        return lib.modelConfiguration();
+    }
+};
